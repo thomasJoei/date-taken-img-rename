a322da516f3643a7f8dbc14532f046eab97fe446
Thomas Joeisseint <thomas.joeisseint@gmail.com>
2020-04-01 23:15:52 -0400

    Update README. Release version.
    Former-commit-id: 31801e1eda5acfab217265b88a1fcd1925963044

--- make-package.sh
+++ make-package.sh
@@ -2,10 +2,10 @@
 mvn clean package
 
 # package for distribution, use jdk 15
-jpackage -i ./target/ \
+jpackage -i ./target/ --dest ./dist \
 --main-class com.thojoeis.Main \
 --icon src/main/resources/rename_box_icon.icns \
---main-jar image-rename-1.0-SNAPSHOT-jar-with-dependencies.jar \
+--main-jar image-rename-1.0-jar-with-dependencies.jar \
 --name "Image Renamer"
 
 
